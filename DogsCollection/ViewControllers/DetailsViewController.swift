ec08350fd93286654eee4f1f31ee300f98597c0e
Yury <quasaryy@gmail.com>
2023-01-28 18:11:10 +0400

    Added share option

--- DogsCollection/ViewControllers/DetailsViewController.swift
+++ DogsCollection/ViewControllers/DetailsViewController.swift
@@ -10,16 +10,24 @@ import UIKit
 class DetailsViewController: UIViewController {
     
     var image: String!
-
+    
     @IBOutlet var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         imageView.image = UIImage(named: image)
         imageView.contentMode = .scaleAspectFill
     }
-
+    
     @IBAction func shareButtonAction(_ sender: UIButton) {
+        let shareController = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: nil)
+        
+        present(shareController, animated: true)
+        
+        shareController.completionWithItemsHandler = {_, bool, _, _ in
+            if bool { print("Success!") }
+        }
     }
+    
 }
